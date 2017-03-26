ac7bbea4205d4af71a52a9b1a399b291d97a01cb
Maged Ahmed <maged.ahmed10@hotmail.com>
2017-03-26 09:21:38 +0200

    control curtains with IRremote

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,11 @@
+#include "IRremote.h"
+
+/*-----( Declare Constants )-----*/
+int receiver = 11; // pin 1 of IR receiver to Arduino digital pin 11
 
 /*-----( Declare objects )-----*/
-
-
+IRrecv irrecv(receiver);           // create instance of 'irrecv'
+decode_results results;            // create instance of 'decode_results'
 /*-----( Declare Variables )-----*/
 
 
@@ -14,13 +18,22 @@ void setup(){
   pinMode(7, OUTPUT);
   Serial.begin(9600);
 
+  // Serial.println("IR Receiver Raw Data + Button Decode Test");
+  irrecv.enableIRIn(); // Start the receiver
+
 
 
 }
 
 void loop(){
 
-
+if (irrecv.decode(&results)) // have we received an IR signal?
+{
+  Serial.println(results.value, HEX);  //UN Comment to see raw values
+  translateIR(); 
+  irrecv.resume(); // receive the next value
+}  
+  
 if (Serial.available() > 0)
 
 {
@@ -78,3 +91,43 @@ if(c == 's'){
 c='\0';
 
 }
+
+
+
+/*-----( Declare User-written Functions )-----*/
+void translateIR() // takes action based on IR code received
+
+// describing Car MP3 IR codes 
+
+{
+
+  switch(results.value)
+
+  {
+
+  case 0xFF02FD:  
+ //  Serial.println(results.value, HEX);
+   digitalWrite(2, HIGH);
+   digitalWrite(7,  LOW);
+   analogWrite(5, 255);
+    delay(3000);
+    analogWrite(5, 0);
+    break;
+
+  case 0xFFC23D:  
+  // Serial.println(results.value, HEX);
+    digitalWrite(2, LOW);
+   digitalWrite(7,  HIGH);
+   analogWrite(5, 255);
+   delay(3000);
+    analogWrite(5, 0);
+    break;
+  default: 
+    Serial.println(" other button   ");
+
+  }
+
+  delay(500);
+
+
+} //END translateIR
